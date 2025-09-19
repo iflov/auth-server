8a60477d6ef9e4bb09cb58e37d108cbb69c888a3
hyeontae.lee <hyeontae.lee@inhuman-z.com>
2025-09-19 18:06:34 +0900

    feat: oauth.module.ts update Dependency Injection

--- src/oauth/oauth.module.ts
+++ src/oauth/oauth.module.ts
@@ -4,10 +4,16 @@ import { OauthController } from './oauth.controller';
 import { OauthService } from './oauth.service';
 import { OAuthClientRepository } from './repository/oauthClient.repository';
 import { AuditLogsRepository } from './repository/auditLogs.repository';
+import { AuthCodesRepository } from './repository/authCodes.repository';
 
 @Module({
   controllers: [OauthController],
-  providers: [OauthService, OAuthClientRepository, AuditLogsRepository],
+  providers: [
+    OauthService,
+    OAuthClientRepository,
+    AuditLogsRepository,
+    AuthCodesRepository,
+  ],
   exports: [],
 })
 export class OauthModule {}
